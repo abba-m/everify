4ccf242bbec28a169149b6b4f180a25c9de74f50
abba-m <mmuhammadabba@gmail.com>
2021-07-13 15:23:34 +0100

    Added the admin autoopen flag and set to true

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,8 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
+  admin: {
+    url: env("PUBLIC_ADMIN_URL", "/dashboard"),
+    autoOpen: true,
+    serveAdminPanel: env.bool("SERVE_ADMIN", true),
+  },
 });
