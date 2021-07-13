61da82d006207659d513d69b2f3643aef0fce44f
abba-m <mmuhammadabba@gmail.com>
2021-07-13 20:10:26 +0100

    Removed Yarn lock file

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,3 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
-  admin: {
-    url: env("PUBLIC_ADMIN_URL", "/dashboard"),
-    autoOpen: true,
-    serveAdminPanel: env.bool("SERVE_ADMIN", true),
-  },
 });
